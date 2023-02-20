54c7f923470dead4d8929eb3959a22828c00159b
leskof <yavuzbuldag@gmail.com>
2023-02-20 11:22:34 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Java Loop Project 14 [patika.dev](https://app.patika.dev/courses/java101/odev-asal-sayi)
+___
+## Description
+This program provides detect prime numbers between 1-100.
+___
+_This program created by [@leskof](https://github.com/leskof)_
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+
+        int counter = 0;
+        for (int i = 2; i <= 100; i++){
+            for (int j = 1; j < i; j++){
+                if (i % j == 0){
+                    counter++;
+                }
+            }
+            if (counter < 2){
+                System.out.print(i + ",");
+            }
+            counter = 0;
+            }
+        }
+    }
+
